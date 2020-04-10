5e7fd9504bd026189c4eab6305c473667f46f8d7
Paradorn Pimporn <paraz@Paradorns-MacBook-Pro.local>
2020-04-10 16:00:36 +0700

    3:Creating <Cell>

--- src/App.js
+++ src/App.js
@@ -2,27 +2,33 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+class Cell extends Component {
+  render() {
+    return <div className="cell">{this.props.number}</div>;
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="board">
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
-          <div className="cell">1</div>
+          <Cell number={2} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
+          <Cell number={4} />
         </div>
       </div>
     );
